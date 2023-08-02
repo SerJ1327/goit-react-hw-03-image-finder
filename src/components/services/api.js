04fb36ba1f889ff962014666089ce640ca885703
Serhii Podhornyi <serhii.podhornyi@gmail.com>
2023-08-02 13:27:38 +0300

    move images. map from ImageGaleryItem to ImageGallery

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -6,7 +6,6 @@ const API_KEY = '36802849-b7af5cd62cfcc85474a5247b9';
 
 const fetchImages = async (query, page, perPage) => {
   const { data } = await axios.get(
-   
     `?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
   );
 
